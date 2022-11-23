d5204706c3fb32482a31a30b91b47fd70c3bb1f6
Sneha-Va <sneha.varghese@nestgroup.net>
2022-11-23 15:54:51 +0530

    first

---
+++ src/Book.java
@@ -0,0 +1,55 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class Book {
+    public static void main(String args[])
+    {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/librarydb","root","");
+
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+        int choice;
+        String bookname,author,category,eastablishdate;
+        int bookchargeperday;
+        Scanner s= new Scanner(System.in);
+        while(true){
+            System.out.println("1.select any option");
+            System.out.println("1.insert");
+            System.out.println("2.select");
+            System.out.println("3.search");
+            System.out.println("4.update");
+            System.out.println("5.delete");
+            System.out.println("6.exit");
+            System.out.println("enter the choice");
+            choice = s.nextInt();
+            switch (choice)
+            {
+                case 1:
+                    System.out.println("insertdata");
+
+                case 2:
+                    System.out.println("select data");
+                case 3:
+                    System.out.println("search data");
+
+                case 4:
+                    System.out.println("update data");
+
+                case 5:
+                    System.out.println("delete data");
+
+                case 6:
+                    System.out.println("exit");
+                    System.exit(0);
+                    break;
+            }
+        }
+    }
+}
+
+
+
